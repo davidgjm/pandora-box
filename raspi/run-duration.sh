a5d7431e2f0369b6d514585e6ef5fd1c266228ff
高建民 <david@gaojianmindeMacBook-Pro.local>
2017-04-14 23:40:28 +0800

    changed the second parameter to more generic command

--- raspi/run-duration.sh
+++ raspi/run-duration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# -lt 2 ]; then
  echo "Invalid arguments!"
- echo "Usage: <duration_in_minutes> <script_path>"
+ echo "Usage: <duration_in_minutes> <command>"
  exit 1
 fi
 
@@ -11,9 +11,9 @@ if [ $1 -lt 1 ]; then
   exit 1
 fi
 
-#Check if the script exists in the file system
-if ! test -f $script; then
-  echo "Script not found: $script"
+#Check if the command exists in the file system
+if test -z $2; then
+  echo "Script not found: $command"
   exit 1
 fi
 
@@ -21,12 +21,12 @@ duration_in_seconds=$(($1 * 60))
 start_time=$(date +%s)
 end_time=$(expr $start_time + $duration_in_seconds)
 
-script=$2
+command=$2
 
-echo "About to run script $script for $1 minutes."
+echo "About to run command $command for $1 minutes."
 echo "Script will run from $(date --date=@$start_time) to $(date --date=@$end_time)"
 
 while [ $start_time -lt $end_time ]; do
-  bash -e "$script"
+  `$command`
   let start_time=$(date +%s)
 done
